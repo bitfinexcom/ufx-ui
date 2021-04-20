15d947010d8af53b721891b8fd1dd2e65d016e50
alexstotsky <alexander.stotsky@code-care.pro>
2021-04-20 13:58:25 +0300

    Exported trades constants/reducers/selectors

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -1,1 +1,2 @@
 export * from './book.constants'
+export * as trades from './trades.constants'
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,2 +1,3 @@
 export { default as ws } from './ws.reducer'
 export { default as book } from './book.reducer'
+export { default as trades } from './trades.reducer'
