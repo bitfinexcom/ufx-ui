f5510c2b82deff4a5e8a32afd64b327f141de974
alexstotsky <alexander.stotsky@code-care.pro>
2021-04-21 17:46:48 +0300

    Update trades selectors

--- src/redux/selectors/trades.selectors.js
+++ src/redux/selectors/trades.selectors.js
@@ -1,39 +1,53 @@
 import _get from 'lodash/get'
-import _isUndefined from 'lodash/isUndefined'
+import _isEmpty from 'lodash/isEmpty'
 import _values from 'lodash/values'
 import { createSelector } from 'reselect'
 
+import { SUBSCRIPTION_CONFIG } from '../constants/trades.constants'
 import { getUfxState } from './common'
+import { getWSChannels, findMatchingChannel } from './ws.selectors'
 
 const EMPTY_OBJ = {}
 
 export const getTrades = (state) => _get(getUfxState(state), 'trades', EMPTY_OBJ)
 
 // symbol : ticker symbol/key i.e. tBTCUSD, TDUSK:USD, tXAUT:UST
-export const getTrade = createSelector(
+export const getTradeForChannelID = createSelector(
   [
     getTrades,
+    getWSChannels,
     (_, symbol) => symbol,
   ],
-  (trades, symbol) => (
-    trades[symbol]
-  ),
+  (trades, wsChannels, symbol) => {
+    const tradesChannel = findMatchingChannel(wsChannels, {
+      ...SUBSCRIPTION_CONFIG,
+      symbol,
+    })
+    const chanId = _get(tradesChannel, 'chanId')
+
+    return _get(trades, [chanId], EMPTY_OBJ)
+  },
 )
 
-export const hasFetchedTrades = (state, symbol) => !_isUndefined(getTrade(state, symbol))
+export const hasFetchedTrades = (state, symbol) => !_isEmpty(getTradeForChannelID(state, symbol))
 
 export const getRecentTrades = createSelector(
-  [getTrade],
-  (trades) => {
+  [
+    getTradeForChannelID,
+    (_, symbol) => symbol,
+  ],
+  (trades, symbol) => {
     if (!trades) {
       return undefined // loading state
     }
-    return _values(trades).reverse()
+
+    return _values(trades[symbol]).reverse()
   },
 )
 
 export default {
   getTrades,
-  getTrade,
   getRecentTrades,
+  hasFetchedTrades,
+  getTradeForChannelID,
 }
