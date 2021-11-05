5656c05a905312d24003fa9320a55998075cb36c
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-11-05 17:22:29 +0200

    Tests: authTrades completed

--- packages/bfx-containers/src/redux/reducers/authTrades.reducer.spec.js
+++ packages/bfx-containers/src/redux/reducers/authTrades.reducer.spec.js
@@ -1,9 +1,10 @@
 import types from '../constants/authTrades.constants'
+import wsTypes from '../constants/ws.constants'
 import authTradesReducer, { INITIAL_STATE } from './authTrades.reducer'
 
 describe('REDUCER: authTrades', () => {
   describe('action: FETCH_AUTH_TRADES_HISTORY_SUCCESS', () => {
-    it('empty payload', () => {
+    it('full payload', () => {
       const action = {
         type: types.FETCH_AUTH_TRADES_HISTORY_SUCCESS,
         payload: [
@@ -43,24 +44,94 @@ describe('REDUCER: authTrades', () => {
       })
     })
   })
-  describe('action: TE_MESSAGE & TU_MESSAGE', () => {
+  describe('action: TE_MESSAGE', () => {
     const initialState = {
       ...INITIAL_STATE,
-      wallet: {
-        BTC: {
-          exchange: {
-            available: 3,
-            total: 5,
-          },
+      tBTCUSD: {
+        399251013: {
+          id: 399251013,
+          pair: 'tETHUSD',
+          mts: 1573485493000,
+          order_id: 33963608932,
+          amount: 0.26334268,
+          price: 187.37,
+          order_type: 'LIMIT',
+          order_price: null,
+          maker: -1,
+          fee: -0.09868591,
+          fee_currency: 'USD',
         },
       },
     }
     it('null payload', () => {
       const action = {
-        type: types.TE_MESSAGE,
+        type: wsTypes.TE_MESSAGE,
         payload: null,
       }
       expect(authTradesReducer(initialState, action)).toEqual(initialState)
+    })
+    it('channelId is not 0', () => {
+      const action = {
+        type: wsTypes.TE_MESSAGE,
+        payload: [
+          5,
+          'te',
+          [
+            399251013,
+            'tETHUSD',
+            1573485493000,
+            33963608932,
+            0.26334268,
+            187.37,
+            'LIMIT',
+            null,
+            -1,
+            -0.09868591,
+            'USD',
+          ],
+        ],
+      }
+      expect(authTradesReducer(initialState, action)).toEqual(initialState)
+    })
+    it('full payload', () => {
+      const action = {
+        type: wsTypes.TE_MESSAGE,
+        payload: [
+          0,
+          'te',
+          [
+            399251013,
+            'tETHUSD',
+            1573485493000,
+            33963608932,
+            0.26334268,
+            187.37,
+            'LIMIT',
+            null,
+            -1,
+            -0.09868591,
+            'USD',
+          ],
+        ],
+      }
+      expect(authTradesReducer(initialState, action)).toEqual({
+        ...initialState,
+        tETHUSD: {
+          399251013: {
+            id: 399251013,
+            pair: 'tETHUSD',
+            mts: 1573485493000,
+            order_id: 33963608932,
+            amount: 0.26334268,
+            price: 187.37,
+            order_type: 'LIMIT',
+            order_price: null,
+            maker: -1,
+            fee: -0.09868591,
+            fee_currency: 'USD',
+          },
+        },
+      })
     })
   })
 })
