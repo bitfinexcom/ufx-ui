c23db8ed9b7e22e10a6901eacd37529083efbe09
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-12-24 17:13:49 +0200

    SortButton: added to storybook

---
+++ packages/core/src/components/ui/SortButton/stories/SortButton.stories.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import {
+  showTemplateStory,
+  getDefaultMetadata,
+} from '../../../../../../storybook/.storybook/helper'
+import { Button } from '../../Button/Button'
+import { SortableButton } from '../SortButton'
+
+export default getDefaultMetadata(
+  SortableButton,
+  'Components/ui/SortableButton',
+  {},
+  true,
+)
+
+const props = {
+  sortAscending: true,
+}
+
+const Component = ({ sortAscending }) => {
+  const [isAsc, setIsAsc] = useState(sortAscending)
+
+  const toggleSorting = () => setIsAsc(!isAsc)
+
+  return (
+    <SortableButton
+      sortAscending={isAsc}
+      content={<Button>Balance</Button>}
+      onSortClick={toggleSorting}
+    />
+  )
+}
+
+export const basic = showTemplateStory(Component, props)
