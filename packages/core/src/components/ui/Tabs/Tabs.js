9dbff8716b2c226e4d7989d27b81b1f5684d9dd0
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-12-27 18:16:05 +0200

    Tabs: added prop description

--- packages/core/src/components/ui/Tabs/Tabs.js
+++ packages/core/src/components/ui/Tabs/Tabs.js
@@ -49,6 +49,7 @@ const Tabs = forwardRef(function Tabs(props, ref) {
 Tabs.propTypes = {
   /**
    * The tabs of the Tabs.
+   * Tab object should has required params (strings): id , title.
    */
   // tabs: { type: { name: 'object', required: false } },
   tabs: PropTypes.arrayOf(PropTypes.shape(TAB_PROP_TYPE)),
