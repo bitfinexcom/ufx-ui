fada50bc6ee12e5d72233297e190a0af72daf03f
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-12-28 08:56:43 +0200

    Storybook: edit config

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,20 +1,8 @@
 module.exports = {
   stories: [
     './stories/*.stories.mdx',
-    '../../core/src/components/ui/**/*.stories.js',
-    '../../core/src/components/Book/**/*.stories.js',
-    '../../bfx-containers/src/containers/Book/**/*.stories.js',
-    '../../bfx-containers/src/containers/Chart/**/*.stories.js',
-    '../../core/src/components/OrderHistory/**/*.stories.js',
-    '../../bfx-containers/src/containers/OrderHistory/**/*.stories.js',
-    '../../core/src/components/Trades/**/*.stories.js',
-    '../../bfx-containers/src/containers/Trades/**/*.stories.js',
-    '../../core/src/components/Ticker/**/*.stories.js',
-    '../../bfx-containers/src/containers/Ticker/**/*.stories.js',
-    '../../core/src/components/TickerList/**/*.stories.js',
-    '../../bfx-containers/src/containers/TickerList/**/*.stories.js',
-    '../../core/src/components/ui/VirtualTable/**/*.stories.js',
-    '../../core/src/components/format/**/*.stories.js',
+    '../../core/src/components/**/**/*.stories.js',
+    '../../bfx-containers/src/containers/**/**/*.stories.js',
   ],
   addons: [
     '@storybook/preset-scss',
