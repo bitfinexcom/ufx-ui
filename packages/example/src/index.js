7965076a348b48f220ca9afafde34dc0bbb1efb4
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-12-28 17:55:37 +0530

    example-app: fix lint

--- packages/example/src/index.js
+++ packages/example/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App'
+import App from "./App";
 
 // import { version  } from '../package.json'
 // import { version as ufxuicoreVersion } from '../node_modules/ufx-ui/package.json'
@@ -11,4 +11,4 @@ import App from './App'
 // console.log('ufx-ui: ', ufxuicoreVersion);
 /* eslint-enable no-console */
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
