d32fc2db67aae9667be8dd2b2bde246a28840038
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-01-05 14:27:58 +0200

    Fix stories

--- packages/core/src/components/ui/DateTimePicker/stories/DateTimePicker.stories.js
+++ packages/core/src/components/ui/DateTimePicker/stories/DateTimePicker.stories.js
@@ -4,10 +4,10 @@ import DateTimePicker from '../DateTimePicker'
 export default getDefaultMetadata(DateTimePicker, 'Components/ui/DateTimePicker', {}, true)
 
 const props = {
-  value: new Date(),
+  value: new Date(1641385191379),
   label: 'Choose date:',
   id: '111222333',
-  maxDate: new Date(),
+  maxDate: new Date(1641385191379),
 }
 
 export const basic = showTemplateStory(DateTimePicker, props)
