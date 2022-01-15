528eb9f98d0fcb829c023aac215ca07683109e14
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-01-15 20:35:52 +0200

    Balances: added props description

--- packages/core/src/components/Balances/Balances.js
+++ packages/core/src/components/Balances/Balances.js
@@ -82,13 +82,35 @@ export const Balances = (props) => {
 }
 
 Balances.propTypes = {
+  /**
+   * Object with balances data
+   */
   // eslint-disable-next-line react/forbid-prop-types
   balances: PropTypes.object,
+  /**
+   * If true, only currencies with a balance
+   * greater than the threshold  showing
+   */
   hideSmallBalances: PropTypes.bool,
+  /**
+   * Callback, invoked, when user checked
+   * checkbox 'Hide small Balances'
+   */
   setHideSmallBalances: PropTypes.func,
+  /**
+   * The custom field/column mapping for the data.
+   */
   rowMapping: PropTypes.objectOf(PropTypes.shape(DATA_MAPPING)),
+  /**
+   * A value of small balance threshold,
+   * using when hideSmallBalances is true
+   */
   smallBalanceThreshold: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /**
+   * A className of Balances container
+   */
   className: PropTypes.string,
+
   showTransfer: PropTypes.bool,
   handleDepositClick: PropTypes.func,
   handleWithdrawClick: PropTypes.func,
