062c1e636df35bbc54005a7d6038c8ce8eee80fc
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-01-17 18:44:27 +0200

    Format Date: added prop description and span wrapper for showing prop description for storybook

--- packages/core/src/components/format/Date.js
+++ packages/core/src/components/format/Date.js
@@ -1,5 +1,6 @@
 import { pad2, applyTimeZoneOffset } from '@ufx-ui/utils'
 import PropTypes from 'prop-types'
+import React from 'react'
 
 import { useStore } from '../../store'
 
@@ -12,23 +13,30 @@ const DateComponent = (props) => {
   const MM = pad2(date.getMonth() + 1)
   const YY = pad2(date.getFullYear()).toString().substr(-2)
 
-  switch (format) {
-    case 'YY-MM-DD HH:mm:ss': {
-      return `${YY}-${MM}-${DD}`
-    }
-    case 'MM-DD-YY HH:mm:ss': {
-      return `${MM}-${DD}-${YY}`
-    }
-    case 'DD-MM-YY HH:mm:ss': {
-      return `${DD}-${MM}-${YY}`
-    }
-    default: {
-      return date.toLocaleDateString()
+  const insertDate = () => {
+    switch (format) {
+      case 'YY-MM-DD HH:mm:ss': {
+        return `${YY}-${MM}-${DD}`
+      }
+      case 'MM-DD-YY HH:mm:ss': {
+        return `${MM}-${DD}-${YY}`
+      }
+      case 'DD-MM-YY HH:mm:ss': {
+        return `${DD}-${MM}-${YY}`
+      }
+      default: {
+        return date.toLocaleDateString()
+      }
     }
   }
+
+  return <span>{insertDate()}</span>
 }
 
 DateComponent.propTypes = {
+  /**
+   * Timestamp in ms, which should be transformed
+   */
   mts: PropTypes.number.isRequired,
 }
 
