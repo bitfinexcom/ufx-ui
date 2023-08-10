78057218f00d79a5f8a2ff734425fa2c76de438e
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2023-08-10 13:42:48 +0300

    change render method of react-dom

--- packages/example/src/index.js
+++ packages/example/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
@@ -11,4 +11,6 @@ import App from "./App";
 // console.log('ufx-ui: ', ufxuicoreVersion);
 /* eslint-enable no-console */
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App/>);
