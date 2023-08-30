36ae0b9b60feef5790efd59eb51729dc3c70f6cf
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2023-08-30 21:07:30 +0530

    fixing tests, wf build

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -3,5 +3,6 @@ import 'regenerator-runtime/runtime'
 import 'jest-canvas-mock'
 import 'jest-enzyme'
 import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
